6d75470cc6f6d44b56857d14b3a72a42ca5f8a0e
Markiyan Derevetskyi <markiyan07345@gmail.com>
2020-09-14 21:06:04 +0300

    declared methods of List interface

--- src/main/java/app/collections/list/List.java
+++ src/main/java/app/collections/list/List.java
@@ -1,4 +1,17 @@
 package app.collections.list;
 
-public interface List {
+import app.collections.Collection;
+
+public interface List<T> extends Collection<T> {
+
+    void add(int index, T element);
+
+    boolean remove(int index);
+
+    T get(int index);
+
+    T set(int index, T element);
+
+    int indexOf(Object element);
+
 }
